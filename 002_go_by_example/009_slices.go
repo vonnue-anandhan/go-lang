6a20c3eee5a85fc904c0e16a6c0c42e5d4c62f82
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-01-12 13:06:54 +0530

    feat: slices

--- 002_go_by_example/009_slices.go
+++ 002_go_by_example/009_slices.go
@@ -34,4 +34,27 @@ func main() {
 	copy(c, s)
 	fmt.Println("cpy:", c)
 
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+
+	fmt.Println("2d: ", twoD)
+
 }
