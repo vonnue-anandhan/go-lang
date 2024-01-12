37dd24c3cf5370aa94fb13aeeeb957768f7f772a
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-01-12 11:02:09 +0530

    feat: slices

---
+++ 002_go_by_example/009_slices.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	// "slices"
+)
+
+func main() {
+
+	// An uninitialized slice equals to nil and has length 0.
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	// To create an empty slice with non-zero length, use the builtin make
+	s = make([]string, 3)
+
+	// By default a new slice’s capacity is equal to its length
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	// Append
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+}
