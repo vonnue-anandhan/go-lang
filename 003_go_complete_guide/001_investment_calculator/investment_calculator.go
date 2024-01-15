c8be6176854ca49009ae0d3a4576b81774fe96f8
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-01-15 10:49:48 +0530

    feat: refactor declarations

--- 003_go_complete_guide/001_investment_calculator/investment_calculator.go
+++ 003_go_complete_guide/001_investment_calculator/investment_calculator.go
@@ -8,12 +8,17 @@ import (
 func main() {
 	const inflationRate = 2.5
 
-	var investmentAmount float64
-
-	years := 10.0
+	var investmentAmount, years float64
 	expectedReturnRate := 5.5
 
+	fmt.Print("Investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow((1+(expectedReturnRate)/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
