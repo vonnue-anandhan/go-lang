a4290f9cb76d8cc4a63b7494dc1ffb3e4ec76c81
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-01-15 10:45:31 +0530

    feat: variables again

--- 003_go_complete_guide/001_investment_calculator/investment_calculator.go
+++ 003_go_complete_guide/001_investment_calculator/investment_calculator.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	const inflationRate = 2.5
 
-	var futureValue = investmentAmount * math.Pow((1+(expectedReturnRate)/100), years)
+	var investmentAmount float64
+
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow((1+(expectedReturnRate)/100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
