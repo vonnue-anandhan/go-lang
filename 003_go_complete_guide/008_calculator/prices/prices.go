22b1315ac472b6f8f006eee875acf7717764f381
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-01-19 12:33:27 +0530

    feat: defer

--- 003_go_complete_guide/008_calculator/prices/prices.go
+++ 003_go_complete_guide/008_calculator/prices/prices.go
@@ -32,11 +32,12 @@ func (job *TaxIncludedPriceJob) LoadData() error {
 	return nil
 }
 
-func (job *TaxIncludedPriceJob) Process() error {
+func (job *TaxIncludedPriceJob) Process(doneChan chan bool, errorChan chan error) {
 	err := job.LoadData()
 
 	if err != nil {
-		return err
+		errorChan <- err
+		return
 	}
 
 	result := make(map[string]string)
@@ -47,7 +48,9 @@ func (job *TaxIncludedPriceJob) Process() error {
 	}
 
 	job.TaxIncludedPrices = result
-	return job.IOManager.WriteResult(job)
+	job.IOManager.WriteResult(job)
+
+	doneChan <- true
 }
 
 func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
