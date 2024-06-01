85dd2c68d632660919daebc4db56a4b37cf989e4
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-06-01 12:49:19 +0530

    feat: add recover

--- errors/recover.go
+++ errors/recover.go
@@ -1,5 +1,24 @@
 package errors
 
+import "fmt"
+
+func myPanic() {
+	panic("something went wrong")
+}
+
 func TestRecover() {
-	
+	// recover must be called within a deferred function.
+	// When the enclosing function panics, the defer will activate and a recover call within it will catch the panic.
+	defer func() {
+		// The return value of recover is the error raised in the call to panic.
+		if r := recover(); r != nil {
+			fmt.Println("Recovered, error:\n", r)
+			fmt.Println("After recover....")
+		}
+	}()
+
+	myPanic()
+
+	fmt.Println("After myPanic()")
+
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	errors.TestErrors()
+	errors.TestRecover()
 }
