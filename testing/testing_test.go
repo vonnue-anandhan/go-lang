132145147db9f7993d636c558c91ad56b3d06251
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-06-03 15:31:10 +0530

    feat: table tests

--- testing/testing_test.go
+++ testing/testing_test.go
@@ -3,10 +3,32 @@ package testing
 import "testing"
 
 func TestMySum(t *testing.T) {
-	want := 5
-	got := MySum(2, 3)
+	// Simple test
+	// want := 5
+	// got := MySum(2, 3)
 
-	if got != want {
-		t.Errorf("Expected %d, got %d", want, got)
+	// if got != want {
+	// 	t.Errorf("Expected %d, got %d", want, got)
+	// }
+
+	// Table test
+	type test struct {
+		input []int
+		want  int
+	}
+
+	tests := []test{
+		{input: []int{2, 3}, want: 5},
+		{input: []int{-1, 0}, want: -1},
+		{input: []int{-2, -2}, want: -4},
+		{input: []int{-2, 6}, want: 4},
+	}
+
+	for _, test := range tests {
+		got := MySum(test.input...)
+		want := test.want
+		if got != want {
+			t.Errorf("Expected %d, got %d", want, got)
+		}
 	}
 }
